933aae8f5581ff17132423c478a4b2880870f11d
ciprimare <ciprimare@gmail.com>
2015-03-23 11:56:46 +0200

    changes

--- app/src/main/java/com/mychataclient/enums/MessageType.java
+++ app/src/main/java/com/mychataclient/enums/MessageType.java
@@ -4,6 +4,7 @@ package com.mychataclient.enums;
  * Created by ciprian.mare on 3/20/2015.
  */
 public enum MessageType {
+    LOGOUT(0),
     REGISTER_MESSAGE(1),
     LOGIN_MESSAGE(2),
     BROADCAST_MESSAGE(3);
